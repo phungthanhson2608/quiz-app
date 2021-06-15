51d546e6856f85feabe5613b0964a37d8467e25e
phungthanhson2608 <85722968+phungthanhson2608@users.noreply.github.com>
2021-06-15 20:28:58 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { FC , useState} from 'react'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import './App.css';
 import { LoginPage } from './Login/LoginPage';
 import { QuizPage } from './QuizPage/QuizPage';
